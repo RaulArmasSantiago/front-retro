d806ec9443cb685f6bdbc73eac0372179087564e
Raul Armas Santiago <raularmassantiago@gmail.com>
2018-09-20 13:10:10 -0500

    bototnes de slider

--- src/components/Device/Device.js
+++ src/components/Device/Device.js
@@ -94,6 +94,8 @@ class Device extends Component{
                             <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
                         </ol>
                         <div className="carousel-inner">
                             <div className="carousel-item active">
